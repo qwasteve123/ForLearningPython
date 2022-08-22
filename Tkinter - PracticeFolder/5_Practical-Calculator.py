272e86b856f3088a47b6617af3112412089e8b72
qwasteve <waitoau01@gmail.com>
2022-08-22 23:07:22 +0800

    calculator

--- Tkinter - PracticeFolder/5_Practical-Calculator.py
+++ Tkinter - PracticeFolder/5_Practical-Calculator.py
@@ -1,3 +1,4 @@
+import math
 from stat import FILE_ATTRIBUTE_NORMAL
 from tkinter import *
 
@@ -16,13 +17,53 @@ def button_clear():
 def button_add():
     first_number = e.get()
     global f_num
-    f_num = int(first_number)
+    global math
+    math = "addition"
+    f_num = float(first_number)
     e.delete(0,END)
+
+def button_subtract():
+    first_number = e.get()
+    global f_num
+    global math
+    math = "subtraction"
+    f_num = float(first_number)
+    e.delete(0,END)    
+    return
+
+def button_multiply():
+    first_number = e.get()
+    global f_num
+    global math
+    math = "multiplication"
+    f_num = float(first_number)
+    e.delete(0,END)    
+    return
+
+def button_divide():
+    first_number = e.get()
+    global f_num
+    global math
+    math = "division"
+    f_num = float(first_number)
+    e.delete(0,END)    
+    return
 
 def button_equal():
     second_number = e.get()
     e.delete(0,END)
-    e.insert(0,f_num + int(second_number))
+
+    if math == "addition":
+        e.insert(0,f_num + int(second_number))
+    
+    if math == "subtraction":
+        e.insert(0,f_num - int(second_number))
+
+    if math == "multiplication":
+        e.insert(0,f_num * int(second_number))
+
+    if math == "division":
+        e.insert(0,f_num / int(second_number))
 
 
 button_1 = Button(root,text="1",padx=40,pady=20,command=lambda: button_click(1))
@@ -35,7 +76,11 @@ button_7 = Button(root,text="7",padx=40,pady=20,command=lambda: button_click(7))
 button_8 = Button(root,text="8",padx=40,pady=20,command=lambda: button_click(8))
 button_9 = Button(root,text="9",padx=40,pady=20,command=lambda: button_click(9))
 button_0 = Button(root,text="0",padx=40,pady=20,command=lambda: button_click(0))
+
 button_add = Button(root,text="+",padx=30,pady=20,command=button_add)
+button_subtract = Button(root,text="-",padx=30,pady=20,command=button_subtract)
+button_multiply = Button(root,text="*",padx=30,pady=20,command=button_multiply)
+button_divide = Button(root,text="/",padx=30,pady=20,command=button_divide)
 button_equal = Button(root,text="Equal",padx=30,pady=20,command=button_equal)
 button_clear = Button(root,text="Clear",padx=30,pady=20,command=button_clear)
 
@@ -53,6 +98,10 @@ button_9.grid(row=1,column=2)
 
 button_0.grid(row=4, column=0)
 button_add.grid(row=1, column=3)
+button_subtract.grid(row=2, column=3)
+button_multiply.grid(row=3, column=3)
+button_divide.grid(row=4, column=3)
+
 button_equal.grid(row=4, column=1,columnspan=1)
 button_clear.grid(row=4, column=2,columnspan=1)
 
