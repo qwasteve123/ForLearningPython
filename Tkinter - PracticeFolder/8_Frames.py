bebe6a93b5694591dc3a03189571b4d6c523a927
qwasteve <waitoau01@gmail.com>
2022-08-29 20:50:37 +0800

    Add 8_frame

---
+++ Tkinter - PracticeFolder/8_Frames.py
@@ -0,0 +1,17 @@
+from tkinter import *
+from PIL import ImageTk, Image
+
+root = Tk()
+root.title('Image Viewer')
+root.iconbitmap(r"C:\Users\Samsung\Desktop\game.ico")
+
+# frame = LabelFrame(root, text='This is my frame...', padx= 50, pady= 50)
+frame = LabelFrame(root, padx= 50, pady= 50)
+frame.pack(padx=100,pady=100)
+
+b = Button(frame, text="Don't click here")
+b2 = Button(frame, text="and here...")
+b.grid(row=0,column=0)
+b2.grid(row=1,column=1)
+#Can use grid / pack in frame
+root.mainloop()
